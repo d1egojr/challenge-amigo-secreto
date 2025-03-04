3f509286ef7b8d751f5405a3e13920bee47de447
Diego Ruiz <ruizdiegoj@gmail.com>
2025-03-04 17:32:34 -0300

    Actualiza app.js funcion mostrarLista

--- app.js
+++ app.js
@@ -3,17 +3,28 @@ let amigos = [];
 
 function agregarAmigo() {
     let nombreIntroducido = document.getElementById('amigo').value;
-    console.log(typeof(nombreIntroducido));
+    //console.log(typeof(nombreIntroducido));
 
     if (nombreIntroducido == '') {
         alert('Por favor, inserte un nombre.');
     } else {
         amigos.push(nombreIntroducido);
-        console.log(amigos);
+        //console.log(amigos);
         limpiarCaja();
+        mostrarLista();
     }
 }
 
 function limpiarCaja() {
     document.getElementById('amigo').value = '';
+}
+
+function mostrarLista() {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = "";
+    for (i=0; i<amigos.length; i++) {
+        let itemLista = document.createElement('li');
+        itemLista.textContent = amigos[i];
+        lista.appendChild(itemLista);
+    }
 }
